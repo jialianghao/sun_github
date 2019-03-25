381b2e825542f93c791d91c4d58bdf504550097b
sunjialiang <sunjialiang@9fbank.com.cn>
2019-03-26 00:23:33 +0800

    first commit

---
+++ src/test/java/com/imooc/git/GitGithubApplicationTests.java
@@ -0,0 +1,16 @@
+package com.imooc.git;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GitGithubApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
