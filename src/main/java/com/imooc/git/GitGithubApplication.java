cc1bcae23f2c32f26e31b1a5d7be04abb08b0e5e
sunjialiang <sunjialiang@9fbank.com.cn>
2019-03-31 11:31:44 +0800

    dev 提交修改新增test01类

--- src/main/java/com/imooc/git/GitGithubApplication.java
+++ src/main/java/com/imooc/git/GitGithubApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitGithubApplication {
 
 	//haha
+	//再次修改
 	public static void main(String[] args) {
 		SpringApplication.run(GitGithubApplication.class, args);
 	}
